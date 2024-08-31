69392b22028f6fc593fc6eeb4585605c7c3e6157
Nisha0202 <nishajabatunnessa@gmail.com>
2024-08-31 18:29:19 +0600

    comment

--- src/components/commentinput/CommentModal.jsx
+++ src/components/commentinput/CommentModal.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoSend } from "react-icons/io5";
 const CommentModal = ({ blogData, closeModal }) => {
   const [comment, setComment] = useState('');
 
@@ -16,14 +17,14 @@ const CommentModal = ({ blogData, closeModal }) => {
   };
 
   return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light flex">
+    <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light flex m-2">
       <div className="relative p-8 bg-gray-100 dark:bg-gray-700 w-full max-w-md m-auto flex-col flex rounded-md">
         <span className="absolute top-0 right-0 p-4">
           <button onClick={closeModal} className="text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100">
           <IoCloseCircleOutline className='text-xl'/>
           </button>
         </span>
-        <h1 className=" mb-4">Comment on {blogData.title}</h1>
+        <h1 className="text-sm font-light mb-4">Comment on {blogData.title}</h1>
         <p className="text-lg mb-4"></p>
         <textarea
           value={comment}
@@ -34,9 +35,9 @@ const CommentModal = ({ blogData, closeModal }) => {
         />
         <button
           onClick={handleSubmit}
-          className="bg-blue-600 text-white mt-4 px-4 py-2 rounded-md text-sm"
+          className="bg-blue-600 font-medium text-white mt-4 px-4 py-2 rounded text-sm"
         >
-          Submit
+          {/* <IoSend className='text-center text-lg w-full'/> */} Post
         </button>
       </div>
     </div>
@@ -44,31 +45,3 @@ const CommentModal = ({ blogData, closeModal }) => {
 };
 
 export default CommentModal;
-
-
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// let isConnected = false;
-
-// const uri = `mongodb+srv://Admin:Admin0202@cluster0.5cua0xk.mongodb.net/sweetshare?retryWrites=true&w=majority&appName=Cluster0`;
-
-// export const connect = async () => {
-//   mongoose.set('strictQuery', true);
-
-//   if (isConnected) {
-//     console.log('MongoDB is already connected');
-//     return;
-//   }
-
-//   try {
-//     await mongoose.connect(uri);
-
-//     isConnected = true;
-//     console.log('Successfully connected to MongoDB');
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//   }
-// };
