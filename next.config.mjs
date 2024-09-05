f22822540d392f359e1672676c0fe606db3ffdfa
Nisha0202 <nishajabatunnessa@gmail.com>
2024-09-05 12:23:21 +0600

    issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
- 
-//     images: {
-//         remotePatterns: [
-//           {
-//             protocol: 'https',
-//             hostname: 'images.unsplash.com',
-//             pathname: '**',
-//           },
-//         ],
-//       },
-//       eslint: {
-//         ignoreDuringBuilds: true, 
-//       }
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
